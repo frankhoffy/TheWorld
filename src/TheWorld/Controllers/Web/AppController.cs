2bcd26868f3a542f61174f0286a08b430ac3865c
Frank <frankhoffy@outlook.com>
2016-04-25 16:03:38 -0700

    MVC 6 module work

--- src/TheWorld/Controllers/Web/AppController.cs
+++ src/TheWorld/Controllers/Web/AppController.cs
@@ -3,14 +3,59 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TheWorld.Services;
+using TheWorld.ViewModels;
 
 namespace TheWorld.Controllers.Web
 {
     public class AppController : Controller
     {
+		private IMailService _mailService;
+
+		public AppController(IMailService service)
+		{
+			_mailService = service;
+		}
+
         public IActionResult Index()
         {
             return View();
         }
-    }
+
+		public IActionResult About()
+		{
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Contact(ContactViewModel model)
+		{
+			if (ModelState.IsValid)
+			{
+				var email = Startup.Configuration["AppSettings:SiteEmailAddress"];
+
+				if (string.IsNullOrEmpty(email))
+				{
+					ModelState.AddModelError("", "Could not send email, configuration problem.");
+				}
+
+				if (_mailService.SendMail(email,
+					email,
+					$"Contact Page from {model.Name} ({model.Email})",
+					model.Message))
+				{
+					ModelState.Clear();
+
+					ViewBag.Message = "Messag Sent. Thanks!";
+				}
+			}
+
+			return View();
+		}
+	}
 }
