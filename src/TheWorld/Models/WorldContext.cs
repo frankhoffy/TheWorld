d22178303346bc54184cd8c660d2842c7a7d1286
Frank <frankhoffy@outlook.com>
2016-05-10 12:37:22 -0700

    Completed Identity module

--- src/TheWorld/Models/WorldContext.cs
+++ src/TheWorld/Models/WorldContext.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.Data.Entity;
 
 namespace TheWorld.Models
 {
-    public class WorldContext : DbContext
+    public class WorldContext : IdentityDbContext<WorldUser>
     {
 		public WorldContext()
 		{
